aa4867fdcf8e6384ab27778a7a1d20f0bf00484b
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-03-28 13:38:16 +0300

    beckdrop

--- js/modal.js
+++ js/modal.js
@@ -12,4 +12,10 @@
     document.body.classList.toggle("modal-open")
     refs.modal.classList.toggle("is-hidden");
   }
+  refs.modal.addEventListener("click", handleBackdropClick);
+  function handleBackdropClick(event) {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  }
 })();
